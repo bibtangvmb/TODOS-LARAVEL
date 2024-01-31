2199cfb4c4ec92f8965be8284afdd5d02f5dfa15
Bintang <ytvalennz@gmail.com>
2024-01-31 10:17:17 +0700

    rubah navbar

--- database/migrations/2024_01_22_084215_create_kategoris__table.php
+++ database/migrations/2024_01_22_084215_create_kategoris__table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('kategoris_', function (Blueprint $table) {
+        Schema::create('kategoris', function (Blueprint $table) {
             $table->string('kode_kategori')->primary();
             $table->text('nama')->nullable();
             $table->text('ket')->nullable();            
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kategoris_');
+        Schema::dropIfExists('kategoris');
     }
 };
--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,20 +1,21 @@
-<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Bintang</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/menu">Makanan</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/kategori">Kategori</a>
-        </li>
-    </div>
-  </div>
+<nav>
+              <nav class="navbar navbar-expand-lg bg-primary">
+                <div class="container-fluid">
+                  <a class="navbar-brand text-light " href="#">Bintang</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav me-0 mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link text-light active" aria-current="page" href="/">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-light active" aria-current="page" href="/menu">Menu</a>
+                  </li>
+                        <a class="nav-link text-light active" aria-current="page" href="/kategori">Kategori</a>
+                      </li>
+                  </li>                  
+              </div>
+            </div>
 </nav>
